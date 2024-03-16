f06f4be63c34377a6aa663aa8e9ccb08acb9d2ae
nusrat3657 <nusratjahan3657r@gmail.com>
2024-03-16 23:51:43 +0600

    full section completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import './App.css'
 import Banner from './components/Banner/Banner'
 import Cards from './components/Cards/Cards'
-import Cooking from './components/Cooking/Cooking'
 import Navbar from './components/Navbar/Navbar'
 import Title from './components/Title/Title'
 import { ToastContainer, toast } from 'react-toastify';
@@ -10,22 +9,24 @@ import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const [cooking, setCooking] = useState([]);
-  
-  const [newCurrentCooking, setCurrentCooking] =useState([]);
+
+  const [newCurrentCooking, setCurrentCooking] = useState([]);
 
   const handleAddToCook = card => {
-  const isExist = cooking.find(item => item.id === card.id);
-    if(!isExist){
+    const isExist = cooking.find(item => item.id === card.id);
+    if (!isExist) {
       setCooking([...cooking, card])
     }
-    else{
+    else {
       toast('Already exist');
     }
   }
 
-  const handleAddToCurrentCooking = (id) =>{
-    const newCurrentCooking = cooking.find(item => item.id !== id.id);
-    setCurrentCooking(prv=> [...prv, newCurrentCooking]);
+  const handleAddToCurrentCooking = (cook) => {
+    setCurrentCooking([...newCurrentCooking, cook])
+    const cooked = cooking.filter(item => item.id !== cook.id);
+    setCooking(cooked);
+    
   }
 
   return (
@@ -36,70 +37,72 @@ function App() {
       <div className='md:flex mx-28'>
         <Cards handleAddToCook={handleAddToCook}></Cards>
         <div className=" lexend ml-4 border-2 w-[450px] p-5 rounded-xl">
-            <h3 className="text-2xl font-bold text-center
+          <h3 className="text-2xl font-bold text-center
             ">Want to cook: {cooking.length}</h3>
-            <hr className="ml-14 my-5 w-[70%]" />
-            <div className="overflow-x-auto w-[320px]">
-                <table className="table">
-                    <thead>
-                        <tr className="">
-                            <th></th>
-                            <th>Name</th>
-                            <th>Time</th>
-                            <th>Calories</th>
-                        </tr>
-                    </thead>
-                </table>
-            </div>
+          <hr className="ml-14 my-5 w-[70%]" />
+          <div className="overflow-x-auto w-[320px]">
+            <table className="table">
+              <thead>
+                <tr className="">
+                  <th></th>
+                  <th>Name</th>
+                  <th>Time</th>
+                  <th>Calories</th>
+                </tr>
+              </thead>
+            </table>
+          </div>
+          <div className="overflow-x-auto">
+            <table className="table">
+              <tbody>
 
-            {
-                cooking.map((cook, index) =>  <div className="overflow-x-auto">
-                <table className="table">
-                    <tbody>
-                        <tr key={index}>
-                            <th>{index+1}</th>
-                            <td>{cook.name}</td>
-                            <td>{cook.prepTime}</td>
-                            <td>{cook.calories}</td>
-                            <button onClick={() => handleAddToCurrentCooking(cook.id)} className="btn bg-[#0BE58A] py-1 px-6 rounded-full text-black font-bold hover:bg-gray-300 mt-[7%]">Preparing</button>
-                        </tr>
-                    </tbody>
-                </table>
-    
-            </div>)
-            }
-            
-            <h3 className="mt-10 text-2xl font-bold text-center
-            ">Currently cooking: </h3>
-            <hr className="ml-14 my-5 w-[70%]" />
-            <div className="overflow-x-auto w-[320px]">
-                <table className="table">
-                    <thead>
-                        <tr className="">
-                            <th></th>
-                            <th>Name</th>
-                            <th>Time</th>
-                            <th>Calories</th>
-                        </tr>
-                    </thead>
-                </table>
-            </div>
-            <div className="overflow-x-auto">
-                <table className="table">
-                    <tbody>
-            {
-                newCurrentCooking.map(id => (
-                        <tr>
-                            <th></th>
-                            <td>{id.name}</td>
-                            <td>{id.prepTime}</td>
-                            <td>{id.calories}</td>
-                        </tr>
-                    ))
-            }
-            </tbody>
-                </table>
-            </div>
+                {
+                  cooking.map((cook, index) =>
+                    <tr key={index}>
+                      <th>{index + 1}</th>
+                      <td>{cook.name}</td>
+                      <td>{cook.prepTime}</td>
+                      <td>{cook.calories}</td>
+                      <button onClick={() => handleAddToCurrentCooking(cook)} className="btn bg-[#0BE58A] py-1 px-6 rounded-full text-black font-bold hover:bg-gray-300 mt-[7%]">Preparing</button>
+                    </tr>
+                  )
+                }
+              </tbody>
+            </table>
+
+          </div>
+
+          <h3 className="mt-10 text-2xl font-bold text-center
+            ">Currently cooking: {newCurrentCooking.length}</h3>
+          <hr className="ml-14 my-5 w-[70%]" />
+          <div className="overflow-x-auto ">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>Name</th>
+                  <th>Time</th>
+                  <th>Calories</th>
+                </tr>
+              </thead>
+            </table>
+          </div>
+          <div className="overflow-x-auto">
+            <table className="table">
+              <tbody>
+                {
+                  newCurrentCooking?.map((id, index) => (
+                    <tr key={index}>
+                      <th>{index + 1}</th>
+                      <td>{id.name}</td>
+                      <td>{id.prepTime}</td>
+                      <td>{id.calories}</td>
+                    </tr>
+                  ))
+                }
+              </tbody>
+            </table>
+          </div>
         </div>
         <ToastContainer />
       </div>
